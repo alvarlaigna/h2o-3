ff7013ba0eb962ce4b1810ee1b9823306355aabe
wendycwong <wendycwong@yahoo.com>
2017-07-27 09:02:21 -0700

    Added more to runit.

--- h2o-r/tests/testdir_jira/runit_pubdev_4702_algo_max_runtime_secs_large.R
+++ h2o-r/tests/testdir_jira/runit_pubdev_4702_algo_max_runtime_secs_large.R
@@ -6,6 +6,15 @@ source("../../scripts/h2o-r-test-setup.R")
 ######################################################################################
 pubdev_4702_test <-
 function() {
+  test_pass_fail = c()
+  # word2vec
+  words <- h2o.importFile(text8.path, destination_frame = "words", col.names = "word", col.types = "String")
+  reduced = words[1:h2o.nrow(words)/100,1]
+  w2v <- h2o.word2vec(reduced, min_word_freq = 5, vec_size = 50, sent_sample_rate = 0.001,
+  init_learning_rate = 0.025, window_size = 4)
+
+
+  browser()
 
   path <- locate("smalldata/logreg/prostate.csv")
   prostate.hex <- h2o.importFile(path, destination_frame="prostate.hex")
